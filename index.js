668d823f2f058511b38752b784156994cbfd7d01
AR123456 <coloradoz99@gmail.com>
2019-12-16 07:11:54 -0700

    determine value of this

--- index.js
+++ index.js
@@ -10,14 +10,11 @@ class Timer {
 
     //
   }
-  // the value of this inside of class
-  // this one runs with the on click
-  start() {
-    // the value of this inside of the class
-    // this is the value of the button.
-    // dont have access to all the methods
-    console.log(this);
-  }
+
+  // start() {
+  //   // need to get the instance of this in the class, not the button
+  //   console.log(this);
+  // }
 }
 
 const durationInput = document.querySelector("#duration");
@@ -27,4 +24,46 @@ const pauseButton = document.querySelector("#pause");
 const timer = new Timer(durationInput, startButton, pauseButton);
 // this will run when the browser loads
 // it is the instance of the timer class
-timer.start();
+// timer.start();
+// console.log(this);
+// const printThis = () => {
+//   console.log(this);
+// };
+
+// how to determine what this is inisde of an arrow function- line 36 is same oas line 38
+// const colors = {
+//   printColor() {
+//     console.log(this);
+//     const printThis = () => {
+//       console.log(this);
+//     };
+//     printThis();
+//   }
+// };
+// colors.printColor();
+
+////find this using old school bind, call or apply
+
+const printThis = function() {
+  console.log(this);
+};
+
+// // this gets the window
+// printThis();
+
+// // this gets the object
+// printThis.call({ color: "red" });
+
+// all other cases ////// - the period to the left of the function, inthis case colors
+const colors = {
+  printColor() {
+    console.log(this);
+  }
+};
+
+const randomObject = {
+  a: 1
+};
+
+randomObject.printColor = colors.printColor;
+randomObject.printColor();
