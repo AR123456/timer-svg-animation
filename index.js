6b8a0056f2a1a01529d7d3c429df92cd5c7851cb
AR123456 <coloradoz99@gmail.com>
2019-12-16 07:09:27 -0700

    initial set up

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("Hi there!");
+// possible implementation
+// Event listener to watch fo  a click on start button
+// draw full border around the timer
+// start counting down the timer
+// each time timer counts down update border
+// each tiem the timer counds down update the text
+// when count down reaches 0 reset border
+//when count down reaches 0 reset timer and get ready for another roun
