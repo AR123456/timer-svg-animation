114806350decae98cda7bf7d5369c5ab5046222e
AR123456 <coloradoz99@gmail.com>
2019-12-17 06:25:18 -0700

    solve the this issue

--- index.js
+++ index.js
@@ -7,14 +7,10 @@ class Timer {
     this.pauseButton = pauseButton;
 
     this.startButton.addEventListener("click", this.start);
-
-    //
   }
-
-  // start() {
-  //   // need to get the instance of this in the class, not the button
-  //   console.log(this);
-  // }
+  start = () => {
+//
+  };
 }
 
 const durationInput = document.querySelector("#duration");
@@ -22,48 +18,3 @@ const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 
 const timer = new Timer(durationInput, startButton, pauseButton);
-// this will run when the browser loads
-// it is the instance of the timer class
-// timer.start();
-// console.log(this);
-// const printThis = () => {
-//   console.log(this);
-// };
-
-// how to determine what this is inisde of an arrow function- line 36 is same oas line 38
-// const colors = {
-//   printColor() {
-//     console.log(this);
-//     const printThis = () => {
-//       console.log(this);
-//     };
-//     printThis();
-//   }
-// };
-// colors.printColor();
-
-////find this using old school bind, call or apply
-
-const printThis = function() {
-  console.log(this);
-};
-
-// // this gets the window
-// printThis();
-
-// // this gets the object
-// printThis.call({ color: "red" });
-
-// all other cases ////// - the period to the left of the function, inthis case colors
-const colors = {
-  printColor() {
-    console.log(this);
-  }
-};
-
-const randomObject = {
-  a: 1
-};
-
-randomObject.printColor = colors.printColor;
-randomObject.printColor();
