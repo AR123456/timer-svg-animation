5137ace0deec67fac95fc4f8ce9c3cdfc15166dd
AR123456 <coloradoz99@gmail.com>
2019-12-17 06:27:02 -0700

    store the tick method

--- index.js
+++ index.js
@@ -9,22 +9,20 @@ class Timer {
     this.startButton.addEventListener("click", this.start);
   }
   start = () => {
-    //the tick()method will be responsible ofr updating text inisde of the timer
-    // start will be responsible for kicking off start.
-    // if start button is clicked get the durration input
-    // pass that into tick
-    // tick will do work of runnning timere and sending time to dom
     this.tick();
-    // setInterval(this.tick, 1000);
-    //setInterval returns an integer and its ID
-    // need to set this to an instace timer
     this.interval = setInterval(this.tick, 1000);
   };
   tick = () => {
+    // what do we want to do every second
+    // show the time in the dom
+    // decrment the anount of time so that the pause can be called at the correct time
+    // likes time remaining = time remaining - 1 , but where is time remaining stored?
+    // since this app us using user interactions makes more sense to grab out of the DOM  (normaly option 1 would be more desirable and more widely used.)
+    
+
     console.log("tick");
   };
   pause = () => {
-    // clearInterval is another JS built in function
     clearInterval(this.interval);
   };
 }
