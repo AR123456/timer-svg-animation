bafe35649f973e264b9905a1bd36d622e1eb4706
AR123456 <coloradoz99@gmail.com>
2019-12-17 06:25:49 -0700

    starting and pausing timer

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 class Timer {
   constructor(durationInput, startButton, pauseButton) {
     this.durationInput = durationInput;
-
     this.startButton = startButton;
-
     this.pauseButton = pauseButton;
+    // watching for the click events
+    this.pauseButton.addEventListener("click", this.pause);
 
     this.startButton.addEventListener("click", this.start);
   }
   start = () => {
-//
+    //the tick()method will be responsible ofr updating text inisde of the timer
+    // start will be responsible for kicking off start.
+    // if start button is clicked get the durration input
+    // pass that into tick
+    // tick will do work of runnning timere and sending time to dom
+    this.tick();
+    // setInterval(this.tick, 1000);
+    //setInterval returns an integer and its ID
+    // need to set this to an instace timer
+    this.interval = setInterval(this.tick, 1000);
+  };
+  tick = () => {
+    console.log("tick");
+  };
+  pause = () => {
+    // clearInterval is another JS built in function
+    clearInterval(this.interval);
   };
 }
 
